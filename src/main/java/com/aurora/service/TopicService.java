6965b14e0113f9aa905404da3dae319c7ee979ef
AissEr3 <553023755@qq.com>
2024-01-01 11:17:41 +0800

    删除多余模块

--- src/main/java/com/aurora/service/TopicService.java
+++
@@ -1,8 +0,0 @@
-package com.aurora.service;
-
-import com.aurora.model.entity.Topic;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface TopicService extends IService<Topic> {
-
-}
